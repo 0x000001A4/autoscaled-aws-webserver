1b7400e1af98ba966b13e899dcc4a1936ae19e1e
0x000001A4 <yourfavtester@proton.me>
2023-06-16 13:57:38 +0100

    updating cpu tracker

--- scripts/cputracker.sh
+++ scripts/cputracker.sh
@@ -1,4 +1,1 @@
-#!/bin/bash
-top -bn1 | grep "Cpu(s)" | \
-           sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | \
-           awk '{print 100 - $1"%"}' 
+top -bn11 -d 1 | grep '%Cpu(s)' | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | tail -10 | awk '{ sum += $1 } END { print 100 - sum / 10 }'
